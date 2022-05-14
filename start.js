dbb2d97e8290ff00acf4a89cb44535cf3545cde9
Mark Oosterveld <mark@oosterveld.org>
2022-05-14 13:10:18 -0400

    the concept works

--- start.js
+++ start.js
@@ -1,9 +1,27 @@
-console.log('sleeping...');
-await new Promise((r) => setTimeout(r, 10_000));
+import child_process from 'child_process';
+import fs from 'fs';
 
-// import child_process from 'child_process';
-// import fs from 'fs';
-// const repoExists = fs.existsSync(`repos/${repoPath}`);
+// console.log('sleeping...');
+// await new Promise((r) => setTimeout(r, 2_000));
+
+// https://github.com/abendigo/portainer-compose.git
+const username = 'abendigo';
+const repo = 'use-beckylib'; // 'portainer-compose';
+
+const repoPath = `${username}/${repo}`;
+const exists = fs.existsSync(`repos/${repoPath}`);
+
+if (!exists) {
+	child_process.execSync(
+		`git clone https://github.com/${username}/${repo}.git repos/${username}/${repo}`
+	);
+}
+// see https://stackoverflow.com/questions/9589814/how-do-i-force-git-pull-to-overwrite-everything-on-every-pull
+// git fetch origin master
+// git reset --hard FETCH_HEAD
+// git clean -df
+
+// app.get('/repos/clone/:username/:repo', (req, res) => {
 // export default (repoExists = false, username = '', repo = '', branch = 'master') => {
 //   if (!repoExists) {
 //     child_process.execSync(`git clone https://${username}:${process.env.PERSONAL_ACCESS_TOKEN}@github.com/${username}/${repo}.git repos/${username}/${repo}`);
